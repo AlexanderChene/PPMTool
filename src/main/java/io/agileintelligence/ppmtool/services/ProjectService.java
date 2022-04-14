19997a44d8d32b18641cd2a36d4fa52447a406d6
Alex Chen <Alex.chen@walmartlabs.com>
2022-04-14 10:27:20 -0500

    custom exception

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,9 +1,12 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -12,9 +15,15 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateproject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch ( Exception e){
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toLowerCase() + " already exists");
 
+        }
         //Logic
-        return projectRepository.save(project);
+
     }
 
 }
