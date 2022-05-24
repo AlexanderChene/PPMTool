9cbd6d62304d1321ff172d31cd8ca48758a8506b
Alex Chen <Alex.chen@walmartlabs.com>
2022-05-24 11:04:56 -0500

    repos

---
+++ src/main/java/io/agileintelligence/ppmtool/domain/Backlog.java
@@ -0,0 +1,59 @@
+package io.agileintelligence.ppmtool.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Backlog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSquence = 0;
+    private String projectIdentifier;
+
+    //OneToOne with project
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
+    private Project project;
+
+    //OneToMany projecttasks
+
+
+    public Backlog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSquence() {
+        return PTSquence;
+    }
+
+    public void setPTSquence(Integer PTSquence) {
+        this.PTSquence = PTSquence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
